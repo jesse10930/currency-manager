c6299d1a6ad74b0e959564f60998fdfd206171f0
jesse10930 <ronda.jesse@gmail.com>
2021-08-21 15:46:57 -0400

    sort and csv functionality

--- js/app.js
+++ js/app.js
@@ -24,12 +24,9 @@ app.controller('MainController', function ($scope) {
       $scope.cents.length === 0 ||
       $scope.commaDelimiter.length === 0
     ) {
-      alert('Must fill out all fields.');
+      alert('All fields must be filled in.');
       return;
     }
-
-    // Declare new object
-    $scope.newObj = {};
 
     // Round price to hundredths or ones
     $scope.newPrice =
@@ -59,7 +56,8 @@ app.controller('MainController', function ($scope) {
         ? $scope.newSymbol + $scope.priceWithDelimiter
         : $scope.priceWithDelimiter + $scope.newSymbol;
 
-    // Push new price object to format table array
+    // Declar new object and push to formatTable array
+    $scope.newObj = {};
     $scope.newObj.country = $scope.country;
     $scope.newObj.currency = $scope.currency;
     $scope.newObj.price = $scope.finalPriceFormat;
@@ -80,11 +78,10 @@ app.controller('MainController', function ($scope) {
 
   // Delete row button click
   $scope.deleteRow = function (i) {
-    console.log(i);
     $scope.formatTable.splice(i, 1);
   };
 
-  // Sort table by Country
+  // Sort table by Country header click
   $scope.sortOutputTablebyCountry = function () {
     let sortedTable;
     sortedTable = $scope.formatTable.sort((a, b) =>
@@ -93,12 +90,47 @@ app.controller('MainController', function ($scope) {
     $scope.formatTable = sortedTable;
   };
 
-  // Sort table by Currency
+  // Sort table by Currency header Click
   $scope.sortOutputTablebyCurrency = function () {
     let sortedTable;
     sortedTable = $scope.formatTable.sort((a, b) =>
       a.currency > b.currency ? 1 : -1
     );
     $scope.formatTable = sortedTable;
+  };
+
+  // Make CSV file with data on download data click
+  $scope.downloadData = function () {
+    const data = $scope.formatTable;
+    let csvContent = 'data:text/csv;charset=utf-8,';
+    let headers = 'Country,Currency,Price,';
+    let allEntryStrings = '';
+
+    // Loop through data array
+    for (let i = 0; i < data.length; i++) {
+      let entryString = '';
+      let csvPrice = '';
+
+      // Wrap commas in quotes
+      for (let j = 0; j < data[i].price.length; j++) {
+        if (data[i].price[j] === ',') {
+          csvPrice += '","';
+        } else {
+          csvPrice += data[i].price[j];
+        }
+      }
+
+      // Make object entry a string separated by commas
+      entryString +=
+        data[i].country + ',' + data[i].currency + ',' + csvPrice + ',';
+
+      // Combine new entry with previous entries
+      allEntryStrings += entryString + '\r\n';
+    }
+
+    // Combine headers and entries, download to local file
+    csvContent += headers + '\r\n' + allEntryStrings;
+    let encodeUri = encodeURI(csvContent);
+    window.open(encodeUri);
   };
 });
